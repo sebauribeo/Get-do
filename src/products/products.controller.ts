0b00134e7a432027d7a16ee91585375a51d4ae95
sebauribeo <chefsebaurib@gmail.com>
2021-05-18 17:22:34 -0400

    Products Swagger

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateProductDto } from 'src/dto/create-product.dto';
 import { Product } from '../interfaces/product.interface';
 import { ProductsService } from './products.service';
